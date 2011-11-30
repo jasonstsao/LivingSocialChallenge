7f2241987f815399d9b065542922bbdf0cb8b07e
Jason Tsao <jason.s.tsao@gmail.com>
2011-11-29 22:03:18 -0800

    sql files

---
+++ create.sql
@@ -0,0 +1,4 @@
+CREATE TABLE Items( purchaser_name VARCHAR(255), 
+item_description VARCHAR(255), item_price FLOAT(8,2), purchase_count Integer, merchant_address VARCHAR(255),
+merchant_name VARCHAR(255));
+
